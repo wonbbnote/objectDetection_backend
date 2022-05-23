f576fc5a95fb54b90a8ca5b81a5d135f90ead3cf
wonbbnote <wonbb3313@gmail.com>
2022-05-23 11:38:45 +0900

    예측 api DB 저장 필요(예측값까지 도출)

--- main/result.py
+++ main/result.py
@@ -17,7 +17,7 @@ device = "cpu"
 model = models.resnet34(pretrained=True)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 3)
-checkpoint = torch.load('main/static/model/model.pth', map_location=torch.device('cpu'))
+checkpoint = torch.load('main/static/model/path.pth', map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint)
 # model = TheModelClass(*args, **kwargs)
 # model.load_state_dict(torch.load('main/static/model/model.pth', map_location=torch.device('cpu')))
@@ -28,29 +28,35 @@ model.eval()
 # 이미지를 읽어 결과를 반환하는 함수
 def get_prediction(image_bytes):
     image = Image.open(image_bytes).convert('RGB')
+    print(image_bytes)
+    print(image)
     transforms_test = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     image = transforms_test(image).unsqueeze(0).to(device)
-    class_names = ['김수로', '이병헌', '송중기']
-
+    class_names = ['마동석', '이병헌', '김종국']
     with torch.no_grad():
         outputs = model(image)
         _, preds = torch.max(outputs, 1)
-        imshow(image.cpu().data[0], title='예측 결과: ' + class_names[preds[0]])
+        #imshow(image.cpu().data[0], title='예측 결과: ' + class_names[preds[0]])
 
     return class_names[preds[0]]
 
 @blue_result.route('/result', methods=['GET'])
 def result():
     # 1. 폴더에 업로드된 파일을 불러와(최신파일?!)
+    
     filenames = os.listdir('main/static/img/')
     file = filenames[-1]
     file_path = f'main/static/img/{file}'
+    
     # 2. 
+    # image = 'main/static/img/2022-05-23-09-46-11.png'
     output = get_prediction(file_path)
+    print(output)
+
     # 3. 결과값을 DB에 저장
     doc = {
         # 유저아이디 토큰
@@ -58,7 +64,8 @@ def result():
         # 파일 이름
         # 모델 결과값
     }
-    return jsonify({'msg': 'success'})
+    
+    return jsonify({'msg': 'success', 'output': output})
 
 
 
