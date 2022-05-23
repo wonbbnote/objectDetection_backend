43adebd968e9ab9284c1f68d1875d02f5615b95c
J1NU2 <sdrtst@naver.com>
2022-05-23 21:56:01 +0900

    회원가입 및 로그인 제외 삭제

--- main/__init__.py
+++ main/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 from . import signup
 from . import login
-from . import index
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 app.register_blueprint(signup.blue_signup)
 app.register_blueprint(login.blue_login)
-app.register_blueprint(index.blue_index)
